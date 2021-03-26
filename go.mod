917152d289a02e1cccb52401d2196225894d48b1
xiongjianbo <10044375@qq.com>
2021-03-26 16:31:36 +0800

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module log
+module github.com/gobkc/log
 
 go 1.16
--- log.go
+++ log.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"time"
 )
@@ -19,6 +20,19 @@ func Println(param ...interface{}) {
 	newParam = append(newParam, param...)
 	newParam = append(newParam, 0x1B)
 	fmt.Printf("%c[0;30;38m["+Prefix+"]%s "+strings.Repeat("%v ", pLen)+"%c[0m\n", newParam...)
+}
+
+//Fatalln 打印行
+func Fatalln(param ...interface{}) {
+	pLen := len(param)
+	if pLen == 0 {
+		return
+	}
+	newParam := append([]interface{}{}, 0x1B, time.Now().Local().Format("2006-01-02 03:04:05"))
+	newParam = append(newParam, param...)
+	newParam = append(newParam, 0x1B)
+	fmt.Printf("%c[0;30;38m["+Prefix+"]%s "+strings.Repeat("%v ", pLen)+"%c[0m\n", newParam...)
+	os.Exit(0)
 }
 
 //Danger 警告
